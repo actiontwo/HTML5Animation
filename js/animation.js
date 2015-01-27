cbe70e5cd268e4494684d5d90a3e344d094ae8f3
actiontwo <actiontwo@designveloper.com>
2015-01-27 22:22:21 +0700

    finish click current icon

--- js/animation.js
+++ js/animation.js
@@ -6,8 +6,9 @@ $(document).ready(function () {
   var test = $('.iconsp-testStack');
   var tools = $('.iconsp-toolsStack');
   var contentIcon = $('.content-icon ul');
+  var initIndexPosition = 0;
 
-
+  contentIcon.find('li:eq(' + initIndexPosition + ')').addClass('currentContent');
   contentIcon.find('li').css({
     transition: '0.5s all ease-in-out',
     left: '100%',
@@ -44,69 +45,96 @@ $(document).ready(function () {
       wrapperIcon.css({left: '40%' });
     }
   });
+  var runCycle = null;
+  var runContentAnimation = function (index) {
+    contentIcon.find('li').removeClass('currentContent').css({
+      transition: '0.5s all ease-in-out',
+      opacity: 0,
+      position: 'absolute'
+    }).addClass('preContent');
+    contentIcon.find('li:eq(' + index + ')').addClass('currentContent').css({
+      transition: '1s all ease-in-out',
+      opacity: '1',
+      position: 'relative',
+      top: 0,
+      left: 0
+    }).removeClass('preContent');
+    setTimeout(function () {
+      contentIcon.find('li.preContent').css({left: '100%'}).removeClass('preContent');
+    }, 1000);
+  };
+  var runCycleIcon = function () {
+    wrapperIcon.find('.arround-icon i').removeClass('current-position');
+    wrapperIcon.find('.arround-icon i').each(function () {
+      var top = $(this).position().top;
+      var left = $(this).position().left;
+      if (top == 0 && left == '80') {
+        $(this).css({top: '-80px', left: '0'});
+      }
+      if (top == 0 && left == '-80') {
+        $(this).css({top: '80px', left: '0'});
+      }
 
+      if (top == '80' && left == '0') {
+        $(this).css({top: '0', left: '80px'}).addClass('current-position');
+      }
+
+      if (top == '-80' && left == '0') {
+        $(this).css({top: '0', left: '-80px'});
+      }
+
+      var currentPosition = wrapperIcon.find('.current-position').index();
+
+      runContentAnimation(currentPosition);
+
+    });
+  };
+  var cssBotomPosition = {transition: '0.5s all ease-in-out', top: '80px', left: '0'};
+  var cssTopPosition = {transition: '0.5s all ease-in-out', top: '-80px', left: '0'};
+  var cssRightPosition = {transition: '0.5s all ease-in-out', top: '0', left: '80px'};
+  var cssLeftPosition = {transition: '0.5s all ease-in-out', top: '0', left: '-80px'};
+  var calcPosition = function (check) {
+    check++;
+    if (check > 3) {
+      return 0
+    } else
+      return check
+
+  };
+  var setCurrentIconPosition = function (index) {
+    var right = index;
+    var top = calcPosition(right);
+    var left = calcPosition(top);
+    var bottom = calcPosition(left);
+
+    wrapperIcon.find('.arround-icon i:eq(' + right + ')').css(cssRightPosition).addClass('currentPosition');
+    wrapperIcon.find('.arround-icon i:eq(' + top + ')').css(cssTopPosition);
+    wrapperIcon.find('.arround-icon i:eq(' + left + ')').css(cssLeftPosition);
+    wrapperIcon.find('.arround-icon i:eq(' + bottom + ')').css(cssBotomPosition);
+
+  };
   setTimeout(function () {
 
-    monitor.css({
-      transition: '0.5s all ease-in-out', top: '80px', left: '0'}).addClass('bottom-position');
-    load.css({
-      transition: '0.5s all ease-in-out', top: '-80px', left: '0'}).addClass('top-position');
-    post.css({
-      transition: '0.5s all ease-in-out', top: '0', left: '80px'}).addClass('right-position');
-    test.css({
-      transition: '0.5s all ease-in-out', top: '0', left: '-80px'}).addClass('left-position');
+    setCurrentIconPosition(initIndexPosition);
     setTimeout(function () {
       contentIcon.find('li.currentContent').css({opacity: '1'});
-    }, 2000);
-    setInterval(function () {
+    }, 1000);
 
-      wrapperIcon.find('.arround-icon i').removeClass('current-position');
-      wrapperIcon.find('.arround-icon i').each(function () {
-        var top = $(this).position().top;
-        var left = $(this).position().left;
-        if (top == 0 && left == '80') {
-          $(this).css({top: '-80px', left: '0'});
-        }
-        if (top == 0 && left == '-80') {
-          $(this).css({top: '80px', left: '0'});
-        }
-
-        if (top == '80' && left == '0') {
-          $(this).css({top: '0', left: '80px'}).addClass('current-position');
-        }
-
-        if (top == '-80' && left == '0') {
-          $(this).css({top: '0', left: '-80px'});
-        }
-
-        var currentPosition = wrapperIcon.find('.current-position').index();
-
-        contentIcon.find('li').removeClass('currentContent').css({
-          transition: '0.5s all ease-in-out',
-          opacity: 0,
-          position: 'absolute'
-        }).addClass('preContent');
-        contentIcon.find('li:eq(' + currentPosition + ')').addClass('currentContent').css({
-          transition: '1s all ease-in-out',
-          opacity: '1',
-          position: 'relative',
-          top: 0,
-          left: 0
-        }).removeClass('preContent');
-        setTimeout(function () {
-          contentIcon.find('li.preContent').css({left: '100%'}).removeClass('preContent');
-        }, 1000);
-        console.log(currentPosition)
-
-      });
-
+    runCycle = setInterval(function () {
+      runCycleIcon();
     }, 5000);
 
   }, 2500);
 
-  wrapperIcon.find('.arround-icon i').click(function(){
+  wrapperIcon.find('.arround-icon i').click(function () {
     var currenIndex = $(this).index();
-    console.log (currenIndex);
+    wrapperIcon.find('.arround-icon i').css({top: 0, left: 0});
+    clearTimeout(runCycle);
+    setCurrentIconPosition(currenIndex);
+    runContentAnimation(currenIndex);
+    runCycle = setInterval(function () {
+      runCycleIcon();
+    }, 5000);
   });
 
 });
