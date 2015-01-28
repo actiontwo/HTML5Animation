856a020136a980ed3067fc7b4a63a18b144ab5b0
actiontwo <actiontwo@designveloper.com>
2015-01-28 12:21:14 +0700

    finish click toolstack

--- js/animation.js
+++ js/animation.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
   var wrapperIcon = $('.icon-wrapper');
-  var monitor = $('.iconsp-monitorStack');
-  var load = $('.iconsp-loadStack');
-  var post = $('.iconsp-postStack');
-  var test = $('.iconsp-testStack');
-  var tools = $('.iconsp-toolsStack');
-  var contentIcon = $('.content-icon ul');
+  var contentIcon = $('.content-icon ul.sub-content');
+  var mainContentIcon = $('.content-icon ul.main-content');
+  var centerIcon = $('.center-icon i');
   var initIndexPosition = 0;
-
-  contentIcon.find('li:eq(' + initIndexPosition + ')').addClass('currentContent');
-  contentIcon.find('li').css({
+  var cssSetupDefautContent = {
     transition: '0.5s all ease-in-out',
+    '-ms-transition': '0.5s all ease-in-out',
+    '-webkit-transition': '0.5s all ease-in-out',
+    '-moz-transition': '0.5s all ease-in-out',
+    '-o-transition': '0.5s all ease-in-out',
     left: '100%',
     position: 'absolute',
     top: 0,
     opacity: '0'
-  });
+  };
+  contentIcon.find('li:eq(' + initIndexPosition + ')').addClass('currentContent');
+  mainContentIcon.find('li').css(cssSetupDefautContent);
+  contentIcon.find('li').css(cssSetupDefautContent);
 
   contentIcon.find('li.currentContent').css({
     left: '0',
@@ -33,11 +35,14 @@ $(document).ready(function () {
 
   wrapperIcon.css({
     transition: '0.5s all ease-in-out',
+    '-ms-transition': '0.5s all ease-in-out',
+    '-webkit-transition': '0.5s all ease-in-out',
+    '-moz-transition': '0.5s all ease-in-out',
+    '-o-transition': '0.5s all ease-in-out',
     left: left
   });
 
   $(window).resize(function () {
-    console.log('ad');
     if ($(window).width() > 600) {
       wrapperIcon.css({left: '10%' });
     }
@@ -49,11 +54,19 @@ $(document).ready(function () {
   var runContentAnimation = function (index) {
     contentIcon.find('li').removeClass('currentContent').css({
       transition: '0.5s all ease-in-out',
+      '-ms-transition': '0.5s all ease-in-out',
+      '-webkit-transition': '0.5s all ease-in-out',
+      '-moz-transition': '0.5s all ease-in-out',
+      '-o-transition': '0.5s all ease-in-out',
       opacity: 0,
       position: 'absolute'
     }).addClass('preContent');
     contentIcon.find('li:eq(' + index + ')').addClass('currentContent').css({
       transition: '1s all ease-in-out',
+      '-ms-transition': '1s all ease-in-out',
+      '-webkit-transition': '1s all ease-in-out',
+      '-moz-transition': '1s all ease-in-out',
+      '-o-transition': '1s all ease-in-out',
       opacity: '1',
       position: 'relative',
       top: 0,
@@ -68,31 +81,50 @@ $(document).ready(function () {
     wrapperIcon.find('.arround-icon i').each(function () {
       var top = $(this).position().top;
       var left = $(this).position().left;
-      if (top == 0 && left == '80') {
+      if (top == 0 && left == '80')
         $(this).css({top: '-80px', left: '0'});
-      }
-      if (top == 0 && left == '-80') {
+
+      if (top == 0 && left == '-80')
         $(this).css({top: '80px', left: '0'});
-      }
 
-      if (top == '80' && left == '0') {
+      if (top == '80' && left == '0')
         $(this).css({top: '0', left: '80px'}).addClass('current-position');
-      }
 
-      if (top == '-80' && left == '0') {
+      if (top == '-80' && left == '0')
         $(this).css({top: '0', left: '-80px'});
-      }
 
       var currentPosition = wrapperIcon.find('.current-position').index();
-
       runContentAnimation(currentPosition);
 
     });
   };
-  var cssBotomPosition = {transition: '0.5s all ease-in-out', top: '80px', left: '0'};
-  var cssTopPosition = {transition: '0.5s all ease-in-out', top: '-80px', left: '0'};
-  var cssRightPosition = {transition: '0.5s all ease-in-out', top: '0', left: '80px'};
-  var cssLeftPosition = {transition: '0.5s all ease-in-out', top: '0', left: '-80px'};
+  var cssBotomPosition = {
+    transition: '0.5s all ease-in-out',
+    '-ms-transition': '1s all ease-in-out',
+    '-webkit-transition': '1s all ease-in-out',
+    '-moz-transition': '1s all ease-in-out',
+    '-o-transition': '1s all ease-in-out',
+    top: '80px', left: '0'};
+  var cssTopPosition = {
+    transition: '0.5s all ease-in-out',
+    '-ms-transition': '1s all ease-in-out',
+    '-webkit-transition': '1s all ease-in-out',
+    '-moz-transition': '1s all ease-in-out',
+    '-o-transition': '1s all ease-in-out',
+    top: '-80px', left: '0'};
+  var cssRightPosition = {
+    transition: '0.5s all ease-in-out',
+    '-ms-transition': '1s all ease-in-out',
+    '-webkit-transition': '1s all ease-in-out',
+    '-moz-transition': '1s all ease-in-out',
+    '-o-transition': '1s all ease-in-out', top: '0', left: '80px'};
+  var cssLeftPosition = {
+    transition: '0.5s all ease-in-out',
+    '-ms-transition': '1s all ease-in-out',
+    '-webkit-transition': '1s all ease-in-out',
+    '-moz-transition': '1s all ease-in-out',
+    '-o-transition': '1s all ease-in-out', top: '0', left: '-80px'};
+
   var calcPosition = function (check) {
     check++;
     if (check > 3) {
@@ -125,16 +157,50 @@ $(document).ready(function () {
     }, 5000);
 
   }, 2500);
-
+  var hiddenIcon = function () {
+    wrapperIcon.find('.arround-icon i').css({top: 0, left: 0});
+  };
   wrapperIcon.find('.arround-icon i').click(function () {
     var currenIndex = $(this).index();
-    wrapperIcon.find('.arround-icon i').css({top: 0, left: 0});
+    hiddenIcon();
     clearTimeout(runCycle);
     setCurrentIconPosition(currenIndex);
     runContentAnimation(currenIndex);
     runCycle = setInterval(function () {
       runCycleIcon();
     }, 5000);
+  });
+  centerIcon.click(function () {
+    hiddenIcon();
+    clearTimeout(runCycle);
+
+    mainContentIcon.find('li').css({
+      transition: '1s all ease-in-out',
+      opacity: 1,
+      left: 0
+    });
+    contentIcon.find('li').css({
+      opacity: 0
+    });
+
+    setTimeout(function () {
+
+      mainContentIcon.find('li').css({
+        opacity: 0,
+        left: '100%',
+        transition:'0.5s'
+      });
+      contentIcon.find('li.currentContent').css({
+        opacity: 1,
+        left: 0
+      });
+
+      setCurrentIconPosition(0);
+      runCycle = setInterval(function () {
+        runCycleIcon();
+      }, 5000);
+    }, 4000);
+
   });
 
 });
